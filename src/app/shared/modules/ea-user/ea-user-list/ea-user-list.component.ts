d58baf8279749590b6c00e52e0ba4458364ebb38
GunPKate <gun@builk.com>
2022-09-26 15:51:32 +0700

    copy all from userlist to ea-user

--- src/app/shared/modules/ea-user/ea-user-list/ea-user-list.component.ts
+++ src/app/shared/modules/ea-user/ea-user-list/ea-user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'ea-user-list',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EaUserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+  selectedUser: User | undefined;
+  userShow: boolean = false;  
 
+  constructor(private userService:UserService) { }
+  
   ngOnInit(): void {
+    this.users = this.userService.getAllUsers();
+    // this.users = this.getAllUser4();
   }
+
+  onSelect(user:User): void{
+    if(!this.userShow){
+      this.selectedUser = user;
+    }else if(this.userShow){
+      this.selectedUser = undefined;
+    }
+  }
+  
+  // getAllUser4(): User[]{
+  //   return [
+  //     {
+  //       id: '1',
+  //       name: 'g',
+  //       email: 'g@gmail.com',
+  //       phone: '0832224545',
+  //       age: 18,
+  //       isActive: true
+  //     },
+  //     {
+  //       id: '2',
+  //       name: 'g2',
+  //       email: 'g2@gmail.com',
+  //       phone: '0832224845',
+  //       age: 18,
+  //       isActive: true
+  //     },
+  //     {
+  //       id: '3',
+  //       name: 'g3',
+  //       email: 'g3@gmail.com',
+  //       phone: '0832224849',
+  //       age: 18,
+  //       isActive: true
+  //     },
+  //     {
+  //       id: '4',
+  //       name: 'f',
+  //       email: 'f@gmail.com',
+  //       phone: '0832224849',
+  //       age: 18,
+  //       isActive: true
+  //     }
+  //   ];
+  // }
+
 
 }
